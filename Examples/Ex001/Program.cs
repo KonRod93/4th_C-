409411cd48b459ab2d7851a23f4e67bed70041ff
Konstantin <rodin.konstantin2014@yandex.ru>
2022-10-29 11:45:47 +1000

    Ex001

---
+++ Examples/Ex001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А");
+double numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+double numberB = Convert.ToInt32(Console.ReadLine());
+double exponentiation = Math.Pow(numberA, numberB);
+Console.WriteLine($"Ответ: {exponentiation}");
